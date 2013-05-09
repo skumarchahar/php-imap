7018ecffe09cd4130f4a6c7b30663bad7fbbba5b
barbushin <barbushin@9e4789b3-c79b-dc01-0daf-bc03078a0b05>
2013-05-09 09:02:12 +0000

    Fix handling destructed IMAP resource

--- ImapMailbox.php
+++ ImapMailbox.php
@@ -34,7 +34,7 @@ class ImapMailbox {
 	public function getImapStream($forceConnection = true) {
 		static $imapStream;
 		if($forceConnection) {
-			if($imapStream && !imap_ping($imapStream)) {
+			if($imapStream && (!is_resource($imapStream) || !imap_ping($imapStream))) {
 				$this->disconnect();
 				$imapStream = null;
 			}
@@ -55,9 +55,8 @@ class ImapMailbox {
 
 	protected function disconnect() {
 		$imapStream = $this->getImapStream(false);
-		if($imapStream) {
-			$this->expungeDeletedMails();
-			imap_close($imapStream);
+		if($imapStream && is_resource($imapStream)) {
+			imap_close($imapStream, CL_EXPUNGE);
 		}
 	}
 
